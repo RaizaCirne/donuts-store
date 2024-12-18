2109b9b8e8d41caade2d4e7ff0de6a78decfb4b9
Raiza Cirne <raizacirne@gmail.com>
2024-12-18 20:07:02 -0300

    feat: add NavBar component to the project

--- donuts-store/src/App.jsx
+++ donuts-store/src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>Olá, Mundo!</h1>
+      <Home />
     </div>
   );
 }
---
+++ donuts-store/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import Logo from "../../assets/img/Logo.svg";
+import { BsCart2 } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-logo-container">
+        <img src={Logo} alt="Logo Donuts" />
+      </div>
+
+      <div className="nav-bar-links-container">
+        <a href="">Home</a>
+        <a href="">Sobre Nós</a>
+        <a href="">Nosso Trabalho</a>
+        <a href="">Avaliação</a>
+        <a href="">Contato</a>
+        <a href="">
+          <BsCart2 className="navbar-cart-icon" />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
