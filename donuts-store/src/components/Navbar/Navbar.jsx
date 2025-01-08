7a1c082b816b72692b5ea24e6af57d7fb813314c
Raíza Cirne <raizacirne@gmail.com>
2025-01-08 11:56:20 -0300

    feat(navbar): add responsive hamburguer menu for mobile

--- donuts-store/src/components/Navbar/Navbar.jsx
+++ donuts-store/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,52 @@
+import { useState } from "react";
 import Logo from "../../assets/img/Logo.svg";
 import "../Navbar/Navbar.css";
 import { BsCart2 } from "react-icons/bs";
+import HomeIcon from "@mui/icons-material/Home";
+import InfoIcon from "@mui/icons-material/Info";
+import HomeWorkIcon from "@mui/icons-material/HomeWork";
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import Drawer from "@mui/material/Drawer";
+import Box from "@mui/material/Box";
+// import { width } from "@fortawesome/free-solid-svg-icons/fa0";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+      icon: <HomeIcon />,
+    },
+    {
+      text: "Sobre Nós",
+      icon: <InfoIcon />,
+    },
+    {
+      text: "Nosso Trabalho",
+      icon: <HomeWorkIcon />,
+    },
+    {
+      text: "Avaliação",
+      icon: <CommentRoundedIcon />,
+    },
+    {
+      text: "Contato",
+      icon: <PhoneRoundedIcon />,
+    },
+    {
+      text: "Cart",
+      icon: <ShoppingCartRoundedIcon />,
+    },
+  ];
+
   return (
     <nav>
       <div className="nav-logo-container">
@@ -21,6 +65,31 @@ const Navbar = () => {
 
         <button className="primary-button">Reservar Agora</button>
       </div>
+
+      {/* mobile */}
+      <div className="nav-bar-menu-container">
+        <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
+      </div>
+
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={() => setOpenMenu(false)}
+          onKeyDown={() => setOpenMenu(false)}
+        >
+          <List>
+            {menuOptions.map((item) => (
+              <ListItem Key={item.text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
     </nav>
   );
 };
