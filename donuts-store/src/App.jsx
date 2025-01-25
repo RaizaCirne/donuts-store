5490cba1e6d99864a4ed458ded9aca8b2a0017d6
Raíza Cirne <raizacirne@gmail.com>
2025-01-25 17:19:51 -0300

    feat: Create Contact component

--- donuts-store/src/App.jsx
+++ donuts-store/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
 import Home from "./components/Home/Home";
 import Testemonial from "./components/Testemonial/Testemonial";
 import Work from "./components/Work/Work";
@@ -11,6 +12,7 @@ function App() {
       <About />
       <Work />
       <Testemonial />
+      <Contact />
     </div>
   );
 }
---
+++ donuts-store/src/components/Contact/Contact.jsx
@@ -0,0 +1,18 @@
+import "../Contact/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-page-wrapper">
+      <p className="primary-subheading">Contacte-nos</p>
+      <h1 className="primary-heading">Tem uma pergunta em mente?</h1>
+      <h2 className="primary-heading">Deixe-nos ajudá-los</h2>
+
+      <div>
+        <input type="text" placeholder="seu_email.com" />
+        <button className="secondary-button">Enviar</button>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
