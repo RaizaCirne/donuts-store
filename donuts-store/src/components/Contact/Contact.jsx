aae0da14312f632d9a9b12f2d60bb0053d4b0b3c
Raíza Cirne <raizacirne@gmail.com>
2025-01-26 20:48:32 -0300

    style: add mobile version of the Contact component

--- donuts-store/src/components/Contact/Contact.jsx
+++ donuts-store/src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
       <h1 className="primary-heading">Tem uma pergunta em mente?</h1>
       <h2 className="primary-heading">Deixe-nos ajudá-los</h2>
 
-      <div>
+      <div className="contact-form-container">
         <input type="text" placeholder="seu_email.com" />
         <button className="secondary-button">Enviar</button>
       </div>
